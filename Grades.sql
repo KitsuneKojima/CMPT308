ca5240e438fb6fa06bf2b32e98a4bc8e9d881ab9
KitsuneKojima <Nikol.Pettine1@marist.edu>
2013-09-17 13:08:25 -0400

    Added tables

---
+++ Grades.sql
@@ -0,0 +1,98 @@
+CREATE Table students 
+(
+   sid character(3) NOT NULL,
+   name varchar(16)
+)
+
+INSERT INTO students (sid, name)
+   VALUES ('s01', 'Simon');
+
+INSERT INTO students (sid, name)
+   VALUES ('s02', 'Alvin');
+   
+INSERT INTO students (sid, name)
+   VALUES ('s03', 'Theo');
+
+INSERT INTO students (sid, name)
+   VALUES ('s04', 'Brittany');
+
+INSERT INTO students (sid, name)
+   VALUES ('s05', 'Janette');
+
+INSERT INTO students (sid, name)
+   VALUES ('s06', 'Elenore');
+
+INSERT INTO students (sid, name)
+   VALUES ('so7', 'Stu');
+
+
+--teachers
+CREATE TABLE teachers
+(
+   tid character(3) NOT NULL,
+   name varchar(25)
+)
+
+INSERT INTO teachers (tid, name)
+   Values ('t01', 'Washington');
+
+INSERT INTO teachers (tid, name)
+   VALUES ('t02', 'Adams');
+
+INSERT INTO teachers (tid, name)
+   VALUES ('t03', 'Jefferson');
+
+INSERT INTO teachers (tid, name)
+   VALUES ('t04', 'Lincdn');
+   
+--Subjects
+CREATE TABLE subjects
+(
+   subid char(4) NOT NULL,
+   name varchar(25)
+)
+
+INSERT INTO subjects (subid, name)
+   VALUES ('sb01', 'History');
+
+INSERT INTO subjects (subid, name)
+   VALUES ('sb02', 'Biology');
+
+INSERT INTO subjects (subid, name)
+   VALUES ('sb03', 'SF');
+   
+--grades
+CREATE TABLE grades
+(
+    gid char(3) NOT NULL primary key,
+    sid character(3) NOT NULL references students(sid),
+    subid character(4) NOT NULL references subjects(sbid),
+    tid character(3) NOT NULL references teachers(tid),
+    grades varchar(5)
+)
+
+INSERT INTO grades (gid, sid, subid, tid, grades)
+   VALUES ('g01', 's01', 'sb01', 't02', 'A');
+   
+INSERT INTO grades (gid, sid, subid, tid, grades)
+   VALUES ('g02', 's01', 'sb02', 't02', 'B');
+   
+INSERT INTO grades (gid, sid, subid, tid, grades)
+   VALUES ('g03', 's07', 'sb03', 't04', 'C++');
+
+INSERT INTO grades (gid, sid, subid, tid, grades)
+   VALUES ('g04', 's07', 'sb02', 't03', 'F');
+   
+INSERT INTO grades (gid, sid, subid, tid, grades)
+   VALUES ('g05', 's06', 'sb01', 't02', 'B+');
+
+INSERT INTO grades (gid, sid, subid, tid, grades)
+   VALUES ('g06', 's02', 'sb03', 't04', 'C');
+
+INSERT INTO grades (gid, sid, subid, tid, grades)
+   VALUES ('g07', 's03', 'sb03', 't04', 'B');   
+  
+
+
+   
+   
