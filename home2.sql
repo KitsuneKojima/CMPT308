f5f88108764b8c5c8463faeb95796f7341c72877
KitsuneKojima <Nikol.Pettine1@marist.edu>
2013-09-21 14:24:27 -0400

    Added Q2 with 3 subqueries

--- home2.sql
+++ home2.sql
@@ -9,3 +9,20 @@ WHERE aid in
    FROM orders
    WHERE orders.cid = 'c002'
  )
+ 
+-- 2
+SELECT DISTINCT pid
+FROM products
+WHERE pid in
+  ( SELECT pid
+    FROM orders
+    WHERE orders.aid in
+      ( SELECT aid
+        FROM orders
+        WHERE orders.cid in
+          (SELECT cid
+           FROM customers
+           WHERE customers.city = 'Kyoto'
+           )
+      )
+  ) 
