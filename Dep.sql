ef1ca63c212c983b10a05cdb48570daa14ced711
KitsuneKojima <Nikol.Pettine1@marist.edu>
2013-10-01 12:22:54 -0400

    fix

--- Dep.sql
+++ Dep.sql
@@ -9,7 +9,7 @@ Create table People(
     pid int not null,
     firstName varchar(20),
     lastName varchar(20),
-    hireDate Date MM/DD/YYYY,
+    hireDate Date,
     Did Varchar(1) not null references Departments(Did),
    primary key(pid)
 );
